fd12f6733e89865f2118fdb5c85fe9cf1e9429bc
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2023-12-10 21:32:40 -0500

    first implementation

---
+++ pagination/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import List, Optional
+
+from fastapi_pagination import Page , add_pagination, paginate
+
+app = FastAPI()
+
+class Comment(BaseModel):
+    comment_id: int
+    content: str
+    author: str
+    timestamp: datetime
+    post_id: int
+    
+class Post(BaseModel):
+    post_id: int
+    content: str
+    author: str
+    timestamp: datetime
+    comments: Optional[List[Comment]] = None
+    
+    
+
+@app.post("/post")
+async def create_post(post: Post):
+    return {"message": "Post created", "post": post}
+
+
+@app.get("/post/{user_id}/{post_id}")
+async def get_post(user_id: int, post_id: int):
+
+    return {"message": "Post fetched", "post_id": post_id}
+
+
+@app.put("/post/{user_id}/{post_comment_id}")
+async def update_post(user_id: int, post_comment_id: int, post: Post):
+    return {"message": "Post updated", "post_id": post_comment_id}
+
+
+@app.delete("/post/{user_id}/{delete_comment_id}")
+async def delete_post_or_comment(user_id: int, delete_comment_id: int):
+    return {"message": "Post or Comment deleted", "id": delete_comment_id}
+
+
+@app.get("/api/posts/{post_id}/comments")
+async def get_post_comments(post_id: int, page: int = 1, limit: int = 10):
+    # right here might need some assitance with pagination
+    return {"message": "Comments fetched", "post_id": post_id}
+
+
+def main():
+  pass
+  
+  
+
+
+
+if __name__ == '__main__':
+  main()
+
+
