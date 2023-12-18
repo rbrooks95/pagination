ab6d1b4e5b109ec65728013c8f87dfebf30450ec
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2023-12-18 02:26:21 -0500

    made dome changes to the structure based on docs

--- pagination/models.py
+++ pagination/models.py
@@ -2,8 +2,7 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-
-Base = declarative_base()
+from database import Base
 
 
 class Comment(Base):
@@ -15,12 +14,16 @@ class Comment(Base):
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
     post_id = Column(Integer, ForeignKey("post.id"))
 
+    items = relationship("Post", back_populates="content")
+
 
 class Post(Base):
-    __tabelname__ = "post"
+    __tabelname__ = "posts"
     id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey("post.id"))
     content = Column(String)
     author = Column(String)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
     comments = Column(String)
+
+    content = relationship("Comment", back_populates="items")
